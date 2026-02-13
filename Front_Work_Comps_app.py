ee92b1291a11a7f08de9315ee81a24b1c4b1d03d
rajanvicky724 <rajanvicky724@gmail.com>
2026-02-13 16:58:25 +0530

    Update Front_Work_Comps_app.py

--- Front_Work_Comps_app.py
+++ Front_Work_Comps_app.py
@@ -281,7 +281,7 @@ if not st.session_state["show_app"]:
         <style>
         .hero-strap {
             background: #22B84D;
-            padding: 70px 0 10px 0;
+            padding: 40px 0 10px 0;
             border-bottom: 1px solid #e0f2e9;
         }
         .hero-strap-inner {
@@ -779,4 +779,5 @@ if subj_file is not None and src_file is not None:
                 st.error(f"An error occurred: {e}")
 else:
     st.info("Please upload both Subject and Data Source Excel files to begin.")
+
 
