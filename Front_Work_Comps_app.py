0ca7095c0ffc2affecf9f0b9754d7f33149a950c
rajanvicky724 <rajanvicky724@gmail.com>
2026-02-13 16:04:28 +0530

    Update Front_Work_Comps_app.py

--- Front_Work_Comps_app.py
+++ Front_Work_Comps_app.py
@@ -287,7 +287,7 @@ if not st.session_state["show_app"]:
             position: relative;
         }
         .logo-wrapper {
-            margin-top: -35px;   /* more negative = logo higher */
+            margin-top: -55px;   /* more negative = logo higher */
             display: flex;
             justify-content: center;
         }
@@ -295,7 +295,7 @@ if not st.session_state["show_app"]:
             font-size: 32px;
             font-weight: 700;
             color: #058f3c;
-            margin-top: 15px;
+            margin-top: -15px;
             margin-bottom: 8px;
             text-align: center;
             font-family: "Segoe UI", sans-serif;
@@ -781,6 +781,7 @@ if subj_file is not None and src_file is not None:
                 st.error(f"An error occurred: {e}")
 else:
     st.info("Please upload both Subject and Data Source Excel files to begin.")
+
 
 
 
